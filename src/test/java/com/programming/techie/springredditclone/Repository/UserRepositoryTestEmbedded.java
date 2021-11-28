508d1b653e31af632110b95c8ea8f7f62bd39da1
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-11-28 18:51:42 +0200

    added ShouldSaveUser function in testing data access layer (database)

---
+++ src/test/java/com/programming/techie/springredditclone/Repository/UserRepositoryTestEmbedded.java
@@ -0,0 +1,37 @@
+package com.programming.techie.springredditclone.Repository;
+
+import com.programming.techie.springredditclone.model.User;
+import com.programming.techie.springredditclone.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class UserRepositoryTestEmbedded {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+
+    public void ShouldSaveUser(){
+        User user = new User(null,"test user","secret password","user@gmail.com", Instant.now(),true);
+        User savedUser = userRepository.save(user);
+        assertThat(savedUser).usingRecursiveComparison().ignoringFields("userId").isEqualTo(user);
+    }
+
+    @Test
+    @Sql("classpath:test-data.sql")
+    public void ShouldSaveUserThroughSqlFile(){
+        Optional<User> test = userRepository.findByUsername("testuser_sql");
+        assertThat(test).isNotEmpty();
+    }
+
+}
